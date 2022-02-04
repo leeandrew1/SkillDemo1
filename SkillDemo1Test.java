db93beabc2bd9bd0ee9abb33b7e6cc7da70024a8
leeandrew1 <andrewlee3250@gmail.com>
2022-02-03 22:54:13 -0800

    Updated test to make it pass

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void checkLength() {
-        assertEquals(2, SkillDemo1.lengthOfString("asdfg"));
+        assertEquals(5, SkillDemo1.lengthOfString("asdfg"));
     }
 }
